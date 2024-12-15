905d51587f2a6f3b60413bd7581e049e6628b61f
Aidan Dozier <aidandozier@yahoo.com>
2024-12-15 12:38:19 -0600

    dcvgvsdgfsdg

---
+++ final/ProductMaintenanceApp/ProductMaintenanceApp/ProductMaintenanceForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ProductMaintenanceApp
+{
+    partial class ProductMaintenanceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnModify = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lstProduct = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnAdd.Location = new System.Drawing.Point(50, 300);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 32);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnModify
+            // 
+            this.btnModify.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnModify.Location = new System.Drawing.Point(195, 300);
+            this.btnModify.Name = "btnModify";
+            this.btnModify.Size = new System.Drawing.Size(124, 32);
+            this.btnModify.TabIndex = 1;
+            this.btnModify.Text = "&Modify";
+            this.btnModify.UseVisualStyleBackColor = false;
+            this.btnModify.Click += new System.EventHandler(this.btnModify_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnRemove.Location = new System.Drawing.Point(340, 300);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(124, 32);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "&Remove";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btnExit.Location = new System.Drawing.Point(640, 300);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(124, 32);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lstProduct
+            // 
+            this.lstProduct.FormattingEnabled = true;
+            this.lstProduct.Location = new System.Drawing.Point(50, 38);
+            this.lstProduct.Name = "lstProduct";
+            this.lstProduct.Size = new System.Drawing.Size(714, 238);
+            this.lstProduct.TabIndex = 4;
+            this.lstProduct.SelectedIndexChanged += new System.EventHandler(this.lstProduct_SelectedIndexChanged);
+            // 
+            // ProductMaintenanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 356);
+            this.Controls.Add(this.lstProduct);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnModify);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "ProductMaintenanceForm";
+            this.Text = "Product Maintenance";
+            this.Load += new System.EventHandler(this.ProjectMaintenanceFormLoad);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnModify;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ListBox lstProduct;
+    }
+}
+
---
+++ final/ProductMaintenanceApp/ProductMaintenanceApp/ProductMaintenanceForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductMaintenanceApp
+{
+    public partial class ProductMaintenanceForm : Form
+    {
+        public ProductMaintenanceForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProjectMaintenanceFormLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstProduct_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
