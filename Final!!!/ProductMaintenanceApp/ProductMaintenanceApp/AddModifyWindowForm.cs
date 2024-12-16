f9b3210c93436016905c98ba42cc16339c4b8fed
RossDozier <ross.dozier@nvent.com>
2024-12-15 18:29:41 -0600

    Kinda Working

--- Final!!!/ProductMaintenanceApp/ProductMaintenanceApp/AddModifyWindowForm.cs
+++ Final!!!/ProductMaintenanceApp/ProductMaintenanceApp/AddModifyWindowForm.cs
@@ -7,16 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProductMaintenanceApp.Models;
 
 namespace ProductMaintenanceApp
 {
     public partial class AddModifyWindowForm : Form
     {
+        public Product Product { get; set; }
+        public bool IsModify { get; set; }
+
         public AddModifyWindowForm()
         {
             InitializeComponent();
         }
 
+        private void AddModifyWindowForm_Load(object sender, EventArgs e)
+        {
+            if (IsModify && Product != null)
+            {
+                tbName.Text = Product.Name;
+                tbVersion.Text = Product.Version.ToString();
+                tbReleseDate.Text = Product.ReleaseDate.ToString("yyyy-MM-dd");
+                tbProductCode.Enabled = false;
+            }
+        }
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -39,17 +53,75 @@ namespace ProductMaintenanceApp
 
         private void btOK_Click(object sender, EventArgs e)
         {
-
+            if (ValidateFields())
+            {
+                if (IsModify)
+                {
+                    UpdateProduct();
+                }
+                else
+                {
+                    AddProduct();
+                }
+                DialogResult = DialogResult.OK;
+                Close();
+            }
         }
 
         private void btCancel_Click(object sender, EventArgs e)
         {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
 
+        private bool ValidateFields()
+        {
+            // Validate input fields and show error messages if necessary
+            if (string.IsNullOrWhiteSpace(tbName.Text))
+            {
+                MessageBox.Show("Please enter the product name.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(tbVersion.Text))
+            {
+                MessageBox.Show("Please enter the product version.");
+                return false;
+            }
+
+            if (!DateTime.TryParse(tbReleseDate.Text, out var releaseDate) ||
+                releaseDate < new DateTime(1753, 1, 1) || releaseDate > new DateTime(9999, 12, 31))
+            {
+                MessageBox.Show("Please enter a valid release date (yyyy-mm-dd).");
+                return false;
+            }
+
+            return true;
         }
 
         private void lbProductCode_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void AddProduct()
+        {
+            Product = new Product
+            {
+                ProductCode = tbProductCode.Text,
+                Name = tbName.Text,
+                Version = decimal.Parse(tbVersion.Text),
+                ReleaseDate = DateTime.Parse(tbReleseDate.Text)
+            };
+        }
+
+        private void UpdateProduct()
+        {
+            Product.Name = tbName.Text;
+            Product.Version = decimal.Parse(tbVersion.Text);
+            Product.ReleaseDate = DateTime.Parse(tbReleseDate.Text);
+        }
     }
 }
+
+
--- Final!!!/ProductMaintenanceApp/ProductMaintenanceApp/ProductMaintenanceForm.cs
+++ Final!!!/ProductMaintenanceApp/ProductMaintenanceApp/ProductMaintenanceForm.cs
@@ -1,3 +1,7 @@
+
+
+using ProductMaintenanceApp.Models;
+
 namespace ProductMaintenanceApp
 {
     public partial class ProductMaintenanceForm : Form
@@ -11,25 +15,110 @@ namespace ProductMaintenanceApp
         {
 
         }
-
-        private void btnAdd_Click(object sender, EventArgs e)
+        private void ProductMaintenanceForm_Load(object sender, EventArgs e)
         {
+            using (var context = new ProductdbMdfContext())
+            {
+                var products = context.Products.OrderBy(p => p.Name).ToList();
+                lstProduct.DataSource = products;
+                lstProduct.DisplayMember = "Name";
+            }
 
         }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (var addModifyForm = new AddModifyWindowForm { IsModify = false })
+            {
+                if (addModifyForm.ShowDialog() == DialogResult.OK)
+                {
+                    using (var context = new ProductdbMdfContext())
+                    {
+                        context.Products.Add(addModifyForm.Product);
+                        context.SaveChanges();
+                        //ProductMaintenanceForm_Load();
+                    }
+                }
+            }
+        }
+
 
         private void btnModify_Click(object sender, EventArgs e)
         {
+            var selectedProduct = (Product)lstProduct.SelectedItem;
+            if (selectedProduct == null)
+            {
+                MessageBox.Show("Please select a product to modify.");
+                return;
+            }
 
+            using (var modifyForm = new AddModifyWindowForm { IsModify = true, Product = selectedProduct })
+            {
+                if (modifyForm.ShowDialog() == DialogResult.OK)
+                {
+                    using (var context = new ProductdbMdfContext())
+                    {
+                        var product = context.Products.Find(selectedProduct.ProductCode);
+                        if (product != null)
+                        {
+                            product.Name = modifyForm.Product.Name;
+                            product.Version = modifyForm.Product.Version;
+                            product.ReleaseDate = modifyForm.Product.ReleaseDate;
+                            context.SaveChanges();
+                            //ProductMaintenanceForm_Load();
+                        }
+                    }
+                }
+            }
         }
 
         private void btnRemove_Click(object sender, EventArgs e)
         {
+            var selectedProduct = (Product)lstProduct.SelectedItem;
+            if (selectedProduct == null)
+            {
+                MessageBox.Show("Please select a product to remove.");
+                return;
+            }
 
+            if (MessageBox.Show($"Are you sure you want to remove {selectedProduct.Name}?", "Confirm Removal", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                using (var context = new ProductdbMdfContext())
+                {
+                    var product = context.Products.Find(selectedProduct.ProductCode);
+                    if (product != null)
+                    {
+                        if (context.Incidents.Any(i => i.ProductCode == product.ProductCode) || context.Registrations.Any(r => r.ProductCode == product.ProductCode))
+                        {
+                            MessageBox.Show("Cannot remove product with related incidents or registrations.");
+                            return;
+                        }
+                        context.Products.Remove(product);
+                        context.SaveChanges();
+                        ProductMaintenanceForm_Load(sender, e);
+                    }
+                }
+            }
         }
+
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-
+            Close();
         }
+
+        
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
